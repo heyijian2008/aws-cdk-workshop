059e68aec3ddd0d0ca7ef17088b30dc7b420ffd4
heyijian2008 <heyijian2008@hotmail.com>
2022-03-11 21:58:49 +1100

    feat: added validation test for readCapacity

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -5,6 +5,15 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
   downstream: lambda.IFunction;
+  
+  /**
+   * The read capacity units for the table
+   *
+   * Must be greater than 5 and lower than 20
+   *
+   * @default 5
+   */
+   readCapacity?: number;
 }
 
 export class HitCounter extends cdk.Construct {
@@ -15,6 +24,11 @@ export class HitCounter extends cdk.Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    
+    if (props.readCapacity !== undefined &&(props.readCapacity < 5 || props.readCapacity >20)) {
+      throw new Error('readCapacity must be greater than 5 and less than 20');
+    }
+    
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
@@ -22,7 +36,8 @@ export class HitCounter extends cdk.Construct {
           name: 'path', 
           type: dynamodb.AttributeType.STRING 
         },
-        encryption: dynamodb.TableEncryption.AWS_MANAGED
+        encryption: dynamodb.TableEncryption.AWS_MANAGED,
+        readCapacity: props.readCapacity ?? 5
     });
     this.table = table;
 
